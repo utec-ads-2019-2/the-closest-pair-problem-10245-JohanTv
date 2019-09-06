90e82fc5f24d8c81dc879e7f667bb51e697a7257
JohanTv <40219652+JohanTv@users.noreply.github.com>
2019-09-05 19:58:07 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,38 +2,67 @@
 #include <cmath>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
 using namespace std;
 
-vector<double> distancias;
-double A1, A2;
-double B1, B2;
-double it;
+class Punto{
+private:
+public:
+    double xi, yi;
+    Punto(double x, double y){
+        xi=x;
+        yi=y;
 
-double calcularDistancia(){
-	double x = sqrt(pow(A1-B1,2)+pow(A2-B2,2));
-    return x;
+    }
+
+    double getXi() const {
+        return xi;
+    }
+
+    double getYi() const {
+        return yi;
+    }
+
+};
+
+double calcularDistancia(Punto* a, Punto* b){
+    return sqrt(pow(a->getXi() - b->getXi(),2)+pow(a->getYi() - b->getYi(),2));
 }
 
 int main() {
-    int numeros=0;
-    while(scanf("%u",&numeros)==1 && numeros!=0){
-        cin>>A1>>A2;
-        for(int i = 1; i<numeros; ++i){
-            cin>>B1>>B2;
-            distancias.push_back(calcularDistancia());
-            A1=B1;
-            A2=B2;
-        }
-        
+    int numPunts;
+    double a, b;
 
-        it =  *min_element(distancias.begin(), distancias.end());
-        
-        if(it<10000) cout<< it << endl;
-        else cout<<"INFINITY"<<endl;
-	
-		
-        distancias.clear();
+    vector<Punto*> puntos;
+    double minDist;
+    double temp;
+    while(cin>>numPunts, numPunts != 0){
+
+        if(numPunts == 1) {
+            cin >> a >> b;
+            cout<<"INFINITY"<<endl;
+        }
+        else {
+            for (int i = 0; i < numPunts; ++i) {
+                cin >> a >> b;
+                puntos.push_back(new Punto(a, b));
+            }
+
+            minDist = calcularDistancia(puntos[0], puntos[1]);
+            for (int i = 0; i < numPunts; ++i) {
+                for (int j = 0; j < numPunts; ++j) {
+                    if(i!=j){
+                        temp = calcularDistancia(puntos[i], puntos[j]);
+                        if (minDist > temp)
+                            minDist = temp;
+                    }
+                }
+            }
+            if (minDist >= 10000) {cout << "INFINITY" << endl;}
+            else {cout << fixed << setprecision(4) << minDist << endl;}
+
+            puntos.clear();
+        }
     }
 
-    return 0;
 }
